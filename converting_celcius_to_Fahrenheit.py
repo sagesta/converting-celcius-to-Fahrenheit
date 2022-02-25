6cf6ad5dc0380da0c4e57572c23badb42aa119ba
seisa <adebodun93@gmail.com>
2022-02-25 14:05:46 +0100

    Add project files.

---
+++ converting_celcius_to_Fahrenheit.py
@@ -0,0 +1,9 @@
+#converting cel to Fah
+cel = float(input("input the value for your temperature in celsius\n"))
+
+fah = (1.8 * cel) + 32
+
+print ('your celsius to fahrenheit is:', fah, "\n")
+
+
+
